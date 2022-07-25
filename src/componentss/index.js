14ed352b1ac36a40866a62151835d77ea4a76347
OloriAsabi <developerqueenlatifa@gmail.com>
2022-07-25 20:14:54 +0100

    initial commit

--- src/componentss/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Loader } from "./Loader";
-export { default as Navbar } from "./Navbar";
-export { default as Footer } from "./Footer";
-export { default as Layout } from "./Layout";
-export { default as About } from './About';
-export { default as ServicesHome } from './ServicesHome';
-export { default as PortfolioHome } from './PortfolioHome';
-export { default as Testimonals } from './Testimonals';
--- src/pagess/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Services } from "./Services";
-export { default as Portfolio } from "./Portfolio";
-export { default as Blog } from "./Blog";
-export { default as Shop } from "./Shop";
-export { default as Contact } from "./Contact";
